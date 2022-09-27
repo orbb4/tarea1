258f25252aa1d251ab7fc38bed203bf3d447882c
cotecita <you@example.com>
2022-09-27 16:41:04 -0300

    sda prueba

--- tarea1/src/tarea1/Tarea1.java
+++ tarea1/src/tarea1/Tarea1.java
@@ -2,6 +2,14 @@
 package tarea1;
 
 class cliente{}
+class Direccion{}
+class OrdenCompra{}
+class docTributario{}
+class Boleta{}
+class Factura{}
+class Pago{}
+class DetalleOrden{}
+class Articulo{}
 
 public class Tarea1 {
 
