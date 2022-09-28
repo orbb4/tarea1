5c38ac885a85c6471254e0f4eafab8f7cc2c0db1
cotecita <marcastillo2021@udec.cl>
2022-09-28 09:59:13 -0300

    clases editadas

--- tarea1/src/tarea1/Tarea1.java
+++ tarea1/src/tarea1/Tarea1.java
@@ -1,15 +1,46 @@
 // testing. si este mensaje se ve en la rama maestra, quizá algo esté mal
 package tarea1;
+import java.util.Date;
 
-class cliente{}
-class Direccion{}
-class OrdenCompra{}
-class docTributario{}
-class Boleta{}
-class Factura{}
+
+class Cliente{
+    private String nombre;
+    private String rut;
+    public Cliente(){}
+    
+}
+class Direccion{
+    public Direccion(){}
+}
+class OrdenCompra{
+    private String estado;
+    private Date fecha;
+    public OrdenCompra(){}
+    public float calcPrecioSinIVA(){}
+    public float calcIVA(){}
+    public float calPrecio(){}
+    public float calcPeso(){}
+}
+class docTributario{
+    private String numero;
+    private String rut;
+    private Date fecha;
+    public docTributario(){}
+}
+class Boleta{
+    public Boleta(){}
+}
+class Factura{
+    public Factura(){}
+}
 class Pago{}
-class DetalleOrden{}
-class Articulo{}
+class DetalleOrden{
+    public DetalleOrden(){}
+}
+class Articulo{
+    public Articulo(){}
+
+}
 
 public class Tarea1 {
 
